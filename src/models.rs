8fe2afa44bd4b7023e81063e27617d35c4b5295c
JustSamAgain <samuel.zoller@protonmail.com>
2025-05-28 23:33:52 +0200

    extracted the models enum into new file for more modularity and added cohere models

---
+++ src/models.rs
@@ -0,0 +1,26 @@
+#![allow(dead_code)]
+
+//you can find all the openai models here: https://platform.openai.com/docs/models
+const SMART_GPT_MODEL: &str = "o4-mini-2025-04-16";
+const BASIC_GPT_MODEL: &str = "gpt-4.1-nano-2025-04-14";
+const SMART_COHERE_MODEL: &str = "command-a-03-2025";
+const BASIC_COHERE_MODEL: &str = "command-r";
+const GEMINI: &str = "gemini-2.5-flash-preview-05-20";
+const TEST_MODEL: &str = "test-model"; //no functionality yet
+pub enum Model {
+    Smart,
+    Basic,
+    Gemini,
+    //TestModel
+}
+
+impl From<Model> for String {
+    fn from(m: Model) -> String {
+        match m {
+            Model::Smart => SMART_COHERE_MODEL.to_string(),
+            Model::Basic => BASIC_COHERE_MODEL.to_string(),
+            Model::Gemini => GEMINI.to_string(),
+            //Model::TestModel => TEST_MODEL.to_string()
+        }
+    }
+}
--- src/task_handler.rs
+++ src/task_handler.rs
@@ -3,29 +3,7 @@
 use url::Url;
 
 use crate::llm::{Completion, LLM, Message, MessageRole};
-
-//you can find all the openai models here: https://platform.openai.com/docs/models
-const SMART_MODEL: &str = "o4-mini-2025-04-16";
-const BASIC_MODEL: &str = "gpt-4.1-nano-2025-04-14";
-const GEMINI: &str = "gemini-2.5-flash-preview-05-20";
-const TEST_MODEL: &str = "test-model"; //no functionality yet
-pub enum Model {
-    Smart,
-    Basic,
-    Gemini,
-    //TestModel
-}
-
-impl From<Model> for String {
-    fn from(m: Model) -> String {
-        match m {
-            Model::Smart => SMART_MODEL.to_string(),
-            Model::Basic => BASIC_MODEL.to_string(),
-            Model::Gemini => GEMINI.to_string(),
-            //Model::TestModel => TEST_MODEL.to_string()
-        }
-    }
-}
+use crate::models::Model;
 
 const INTRO_1: &str = r#"You are an autonomous agent that solves coding tasks. 
 You keep your explanations as concise as possible.
@@ -58,7 +36,7 @@ impl TaskHandler {
         let base_url: String = base_url.clone().into();
         let base_url = format!("{}/chat/completions", base_url);
         TaskHandler {
-            llm: LLM::full(api_key.to_string(), base_url, BASIC_MODEL.to_string()),
+            llm: LLM::full(api_key.to_string(), base_url, Model::Basic.into()),
         }
     }
 
