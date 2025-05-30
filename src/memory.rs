c43c41d9815f9533bcc87ef0dbd8b7ab87a9d273
JustSamAgain <samuel.zoller@protonmail.com>
2025-05-30 14:56:25 +0200

    added URL type

--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use crate::llm::{LLM, MessageRole};
+use url::Url;
 
 const SUMMARIZE_INTRO: &str =
     "Build a memory representation of the interaction. Skip all introduction. 
@@ -9,24 +10,24 @@ along with the latest two messages—one from the user and one from the assistan
 Focus on capturing key facts, goals, preferences, and any evolving context.\n";
 
 pub struct Memory {
-    history: String,
+    history: String, //make option??
     llm: LLM,
     api_key: String,
-    base_url: String,
+    base_url: Url,
 }
 
 impl Memory {
     ///creates new Memory instance with empty history and predefined model
-    pub fn new(api_key: String, base_url: String) -> Self {
+    pub fn new(api_key: String, base_url: &Url) -> Self {
         Memory {
             history: "".to_string(),
             llm: LLM::full(api_key.clone(), base_url.clone(), "model".to_string()), //TODO replace model with one from model enum
             api_key: api_key,
-            base_url: base_url,
+            base_url: base_url.clone(),
         }
     }
     ///creates a new Memory instance with given history and predefined model
-    pub fn new_with_history(api_key: String, base_url: String, history: String) -> Self {
+    pub fn new_with_history(api_key: String, base_url: &Url, history: String) -> Self {
         Self::new(api_key, base_url).with_history(&history)
     }
     ///(re-)sets the current history to the given one
