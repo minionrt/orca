a92c4f091c98b1186be9779d2bd926f39743c837
FrodoBeutlinAliasLeon <leon.krasniq@student.uni-tuebingen.de>
2025-06-05 19:08:24 +0200

    add lib.rs and basic unit test for tool response parsing

---
+++ tests/tool_test.rs
@@ -0,0 +1,37 @@
+
+use teamprojekt_agents::llm::{CompletionKind};
+use teamprojekt_agents::openai::{ToolCall};
+
+#[test]
+    fn parses_only_toolcalls_to_completionkind() {
+        //example response body for tools
+        let json = r#"
+        [
+            {
+                "id": "call_abc123",
+                "type": "function",
+                "function": {
+                    "name": "get_current_weather",
+                    "arguments": "{\"location\": \"Boston, MA\"}"
+                }
+            }
+        ]
+        "#;
+
+        let tool_calls: Vec<ToolCall> = serde_json::from_str(json).unwrap();
+        let completion_kind = CompletionKind::ToolCalls(tool_calls.clone());
+        // run cargo test -- --nocapture to see actual parsing.
+        println!("{:?}", completion_kind);
+
+        // checking if it was parsed right
+        match completion_kind {
+            CompletionKind::ToolCalls(tc) => {
+                assert_eq!(tc[0].function.name, "get_current_weather");
+                assert_eq!(tc[0].function.arguments, "{\"location\": \"Boston, MA\"}");
+            }
+            _ => panic!("Did not parse ToolCalls correctly!"),
+        }
+    }
+
+
+
