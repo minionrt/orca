523797309f8e6644ecd21bd631df43dd95658f60
JustSamAgain <samuel.zoller@protonmail.com>
2025-06-05 23:28:03 +0200

    changed the cohere models

--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,11 @@
 #![allow(dead_code)]
 
 //you can find all the openai models here: https://platform.openai.com/docs/models
+//cohere models: https://docs.cohere.com/v2/docs/models
 const SMART_GPT_MODEL: &str = "o4-mini-2025-04-16";
 const BASIC_GPT_MODEL: &str = "gpt-4.1-nano-2025-04-14";
-const SMART_COHERE_MODEL: &str = "cohere/command-a-03-2025";
-const BASIC_COHERE_MODEL: &str = "cohere/command-r";
+const SMART_COHERE_MODEL: &str = "cohere/command-r7b-12-2024";
+const BASIC_COHERE_MODEL: &str = "cohere/command-a-03-2025";
 const GEMINI: &str = "google-gemini/gemini-2.5-flash-preview-05-20";
 const TEST_MODEL: &str = "test-model"; //no functionality yet
 pub enum Model {
