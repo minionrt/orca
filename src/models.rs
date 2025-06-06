91596df25980e409a5f2aff004e551e7551a4c66
JustSamAgain <samuel.zoller@protonmail.com>
2025-06-06 12:41:00 +0200

    updated models

--- src/models.rs
+++ src/models.rs
@@ -4,7 +4,7 @@
 //cohere models: https://docs.cohere.com/v2/docs/models
 const SMART_GPT_MODEL: &str = "o4-mini-2025-04-16";
 const BASIC_GPT_MODEL: &str = "gpt-4.1-nano-2025-04-14";
-const SMART_COHERE_MODEL: &str = "cohere/command-r7b-12-2024";
+const SMART_COHERE_MODEL: &str = "cohere/command-a-03-2025";
 const BASIC_COHERE_MODEL: &str = "cohere/command-a-03-2025";
 const GEMINI: &str = "google-gemini/gemini-2.5-flash-preview-05-20";
 const TEST_MODEL: &str = "test-model"; //no functionality yet
