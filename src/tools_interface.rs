bf5ff1a274ead765873e0558d924a4b7e1e0681a
JustSamAgain <samuel.zoller@protonmail.com>
2025-06-17 22:26:29 +0200

    formatting

--- src/tools_interface.rs
+++ src/tools_interface.rs
@@ -4,7 +4,7 @@ use crate::openai;
 
 trait ToolInstance {
     // runs the function with its parameters given as Vector of Strings
-    fn run(&self, params: Vec<String>)-> Option<String>;
+    fn run(&self, params: Vec<String>) -> Option<String>;
     // returns the definition and description of the function as defined in openai::Tool
-    fn return_choice()-> openai::Tool;
+    fn return_choice() -> openai::Tool;
 }
