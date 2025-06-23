472eceeb95f9990062fa03a6ced689f3f29652d3
Jakub Schwenkbeck <Jakub@schwenkbeck.de>
2025-06-23 16:43:45 +0200

    make the ToolInstance public to use in other folders

--- src/tools_interface.rs
+++ src/tools_interface.rs
@@ -2,7 +2,7 @@
 
 use crate::openai;
 
-trait ToolInstance {
+pub trait ToolInstance {
     // runs the function with its parameters given as Vector of Strings
     fn run(&self, params: Vec<String>) -> Result<String, Box<dyn std::error::Error>>;
     // returns the definition and description of the function as defined in openai::Tool
