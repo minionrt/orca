7841ad86dc87098707739529bb7d618ff1a45586
FrodoBeutlinAliasLeon <leon.krasniq@student.uni-tuebingen.de>
2025-06-30 11:40:27 +0200

    insert the request from the CI into the task handler - replacing hardcoded request

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ mod memory;
 mod models;
 mod openai;
 mod task_handler;
-
+mod fetch_task;
 use llm::Completion;
+use reqwest::blocking::Client;
 use std::env;
 use task_handler::{Task, TaskHandler, TaskOutcome};
 use url::Url;
 mod report;
 use report::try_report_failure;
+mod repo_clone;
+use crate::fetch_task::get_task;
 
 fn main() {
     println!();
@@ -22,12 +25,22 @@ fn main() {
     let minion_api: Url = env::var("MINION_API_BASE_URL").unwrap().parse().unwrap();
     let minion_token = env::var("MINION_API_TOKEN").unwrap();
 
-    //task handler interaction example
-    //let task_handler = TaskHandler::new_set_model(&minion_token, &minion_api, models::Model::Smart);
+    //Task handler interaction example
+    //Let task_handler = TaskHandler::new_set_model(&minion_token, &minion_api, models::Model::Smart);
     let task_handler = TaskHandler::new_set_model(&minion_token, &minion_api, models::Model::Smart);
-    let task = Task {
-        request: "Please write a simple FizzBuzz program.".to_string(),
+
+    //Create new Client for get_task
+    let client: Client = Client::new();
+
+    //Fetch the raw task data 
+    let raw_task = get_task(&minion_api, &minion_token, client);
+
+    //Match the raw task data to only get the description of the task.
+    let task = match raw_task{
+        Ok(res) =>  Task { request: res.description.to_string() },
+        Err(_res) => Task {request: "There has been an error on passing the task.".to_string()}
     };
+
     let response = task_handler.run(&task);
 
     let _response = match response {
